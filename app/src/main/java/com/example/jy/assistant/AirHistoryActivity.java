316ac16bd402476ee9c0908b0ef89b33d1da45e6
Young <sw002106@gmail.com>
2019-01-30 15:40:09 -0800

    first commit

---
+++ app/src/main/java/com/example/jy/assistant/AirHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.jy.assistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class AirHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_air_history);
+    }
+}
--- app/src/main/java/com/example/jy/assistant/MainActivity.java
+++ app/src/main/java/com/example/jy/assistant/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(MainActivity.this, SignUpActivity.class);
+                intent = new Intent(MainActivity.this, AirHistoryActivity.class);
                 startActivity(intent);
             }
         });
